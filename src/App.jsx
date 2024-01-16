01a6e5ee13164ac08dd3d16c8936d60b9efe5c46
mmednoui <gm_noui@esi.dz>
2024-01-16 21:07:02 +0100

    creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Signin from "./pages/Sign/Signin";
+import Signup from "./pages/Sign/Signup";
+
 function App() {
-  return <div className="text-3xl text-red-300">heeyooo</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
