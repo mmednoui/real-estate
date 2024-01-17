7b046acc7cb4db690540b30edc8b4e331a06bc7a
mmednoui <gm_noui@esi.dz>
2024-01-17 02:01:40 +0100

    creating the header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Signin from "./pages/Sign/Signin";
 import Signup from "./pages/Sign/Signup";
+import Header from "./components/Header";
+import About from "./pages/about/About";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="px-10 py-3 bg-emerald-50 flex items-center justify-center sm:justify-between">
+      <ul className="flex ">
+        <Link to="/">
+          <li className="hidden sm:inline font-bold cursor-pointer p-2 hover:opacity-50 duration-150 ease-out ">
+            Home
+          </li>
+        </Link>
+
+        <Link to="/about">
+          <li className=" hidden sm:inline font-bold cursor-pointer p-2 hover:opacity-50 duration-150 ease-out ">
+            About
+          </li>
+        </Link>
+      </ul>
+      <div className="flex ">
+        <form className="flex items-center">
+          <label for="simple-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+
+            <input
+              type="text"
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search"
+              required
+            />
+          </div>
+        </form>
+        <Link to="/signin">
+          <button class="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded mx-3">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
