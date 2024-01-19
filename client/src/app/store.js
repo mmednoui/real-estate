6db3ec7f5ef5b6ca6c102bf12f7a81c281632253
mmednoui <gm_noui@esi.dz>
2024-01-19 03:26:26 +0100

    sign in persist when reload

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const combinedReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistedConfig, combinedReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
