a8809f7b267db37c6098715c537c9a239bd11130
mmednoui <gm_noui@esi.dz>
2024-01-21 04:21:29 +0100

    listing UPDATE route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyUser, createListing);
 listingRouter.delete("/delete/:id", verifyUser, deleteListing);
+listingRouter.delete("/update/:id", verifyUser, updateListing);
 
 export default listingRouter;
