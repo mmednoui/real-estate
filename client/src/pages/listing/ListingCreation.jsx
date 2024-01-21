c91976fb161d225f0feaaca906420c03c88dc471
mmednoui <gm_noui@esi.dz>
2024-01-21 02:35:12 +0100

    complete the listing create functionality

--- client/src/pages/listing/ListingCreation.jsx
+++ client/src/pages/listing/ListingCreation.jsx
@@ -6,8 +6,12 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../../firebase.js";
-
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function ListingCreation() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [formData, setFormData] = useState({
@@ -15,10 +19,10 @@ function ListingCreation() {
     name: "",
     description: "",
     address: "",
-    type: "rent",
+    type: "sell",
     beds: 0,
     baths: 0,
-    Price: 0,
+    price: 0,
     offer: false,
     parking: false,
     furnished: false,
@@ -26,6 +30,37 @@ function ListingCreation() {
   const [images, setImages] = useState([]);
 
   console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imgUrls.length < 1)
+        return setError("You must upload at least one image");
+
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+        return;
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   const handleImageSubmit = (e) => {
     if (images.length > 0 && images.length + formData.imgUrls.length < 7) {
       setUploading(true);
@@ -51,6 +86,36 @@ function ListingCreation() {
     } else {
       setImageUploadError("You can only upload 6 images per listing");
       setUploading(false);
+    }
+  };
+  const handleChange = (e) => {
+    if (e.target.id === "type") {
+      setFormData({
+        ...formData,
+        type: e.target.value,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
     }
   };
   const handleRemoveImage = (index) => {
@@ -84,7 +149,10 @@ function ListingCreation() {
     });
   };
   return (
-    <form className="p-10 bg-white mx-auto my-10 max-w-3xl">
+    <form
+      onSubmit={handleSubmit}
+      className="p-10 bg-white mx-auto my-10 max-w-3xl"
+    >
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h1 className="text-base font-semibold leading-7 text-gray-900">
@@ -105,11 +173,14 @@ function ListingCreation() {
               <div className="mt-2 ">
                 <div className="flex  rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600  ">
                   <input
+                    onChange={handleChange}
                     type="text"
                     name="name"
                     id="name"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="Name"
+                    value={formData.name}
+                    required
                   />
                 </div>
               </div>
@@ -124,12 +195,16 @@ function ListingCreation() {
               </label>
               <div className="mt-2">
                 <textarea
+                  type="textarea"
+                  onChange={handleChange}
                   id="description"
                   name="description"
                   placeholder="Description"
                   rows={3}
                   className="block pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-full"
                   defaultValue={""}
+                  value={formData.description}
+                  required
                 />
               </div>
               <p className="mt-3 text-sm leading-6 text-gray-600">
@@ -145,11 +220,14 @@ function ListingCreation() {
               </label>
               <div className="mt-2">
                 <input
+                  onChange={handleChange}
                   placeholder="Address"
                   type="text"
                   name="address"
                   id="address"
                   autoComplete="street-address"
+                  value={formData.address}
+                  required
                   className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -167,6 +245,7 @@ function ListingCreation() {
                     >
                       <span>Upload a file</span>
                       <input
+                        required
                         onChange={(e) => setImages(e.target.files)}
                         id="imgUrls"
                         name="imgUrls"
@@ -230,9 +309,11 @@ function ListingCreation() {
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
+                      onChange={handleChange}
                       id="furnished"
                       name="furnished"
                       type="checkbox"
+                      checked={formData.furnished}
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
@@ -252,6 +333,8 @@ function ListingCreation() {
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
+                      onChange={handleChange}
+                      checked={formData.parking}
                       id="parking"
                       name="parking"
                       type="checkbox"
@@ -273,6 +356,8 @@ function ListingCreation() {
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
+                      onChange={handleChange}
+                      checked={formData.offer}
                       id="offer"
                       name="offer"
                       type="checkbox"
@@ -301,6 +386,8 @@ function ListingCreation() {
                 Type Of The Listing
               </label>
               <select
+                onChange={handleChange}
+                value={formData.type}
                 id="type"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
               >
@@ -320,12 +407,15 @@ function ListingCreation() {
                   <div className="mt-2   ">
                     <div className="flex  rounded-md shadow-sm ring-1 ring-inset ring-gray-300   ">
                       <input
+                        onChange={handleChange}
+                        requiredv
                         type="number"
                         name="beds"
                         id="beds"
                         className=" w-1/6 block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                         placeholder="Beds"
                         defaultValue={0}
+                        value={formData.beds}
                       />
                     </div>
                   </div>
@@ -342,12 +432,15 @@ function ListingCreation() {
                   <div className="mt-2 ">
                     <div className="flex  rounded-md shadow-sm ring-1 ring-inset ring-gray-300   ">
                       <input
+                        onChange={handleChange}
+                        required
                         type="number"
                         name="baths"
                         id="baths"
                         className="w-1/6 block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                         placeholder="Baths"
                         defaultValue={0}
+                        value={formData.baths}
                       />
                     </div>
                   </div>
@@ -364,12 +457,15 @@ function ListingCreation() {
                   <div className="mt-2 ">
                     <div className="flex  rounded-md shadow-sm ring-1 ring-inset ring-gray-300   ">
                       <input
+                        onChange={handleChange}
+                        required
                         type="number"
                         name="price"
                         id="price"
                         className=" w-1/6 block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                         placeholder="Price"
                         defaultValue={0}
+                        value={formData.price}
                       />
                     </div>
                   </div>
@@ -388,12 +484,14 @@ function ListingCreation() {
           Cancel
         </button>
         <button
+          disabled={loading || uploading}
           type="submit"
           className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Create
+          {loading ? "Creating..." : "Create listing"}
         </button>
       </div>
+      {error && <p className="text-red-700 text-sm">{error}</p>}
     </form>
   );
 }
